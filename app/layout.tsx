0092d965358cb9309a8bb4c7a178e47dfeac1225
sanyu <aa937583499@gmail.com>
2023-11-20 17:20:30 +0800

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Open_Sans } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+
+const font = Open_Sans({
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Discord-Clone",
+  description: "Full Stack Discord-Clone",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={font.className}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem={false}
+            storageKey="discord-theme"
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
