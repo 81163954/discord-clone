f775e18511d811d6b1863a69dbe1ec3cf5a0ab4f
sanyu <aa937583499@gmail.com>
2024-03-08 18:32:23 +0800

    init chat-messages with chat-welcome

---
+++ components/chat/chat-messages.tsx
@@ -0,0 +1,33 @@
+import { Member } from "@prisma/client";
+import { ChatWelcome } from "./chat-welcome";
+
+interface ChatMessagesProps {
+  member: Member;
+  name: string;
+  chatId: string;
+  apiUrl: string;
+  socketUrl: string;
+  socketQuery: Record<string, string>;
+  paramKey: "channelId" | "conversationId";
+  paramValue: string;
+  type: "channel" | "conversation";
+}
+
+export const ChatMessages = ({
+  member,
+  name,
+  chatId,
+  apiUrl,
+  socketUrl,
+  socketQuery,
+  paramKey,
+  paramValue,
+  type,
+}: ChatMessagesProps) => {
+  return (
+    <div className="flex flex-1 flex-col overflow-y-auto py-4">
+      <div className="flex-1" />
+      <ChatWelcome type={type} name={name} />
+    </div>
+  );
+};
